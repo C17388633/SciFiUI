9d7841dea633660aa7562d7ef88316bd7b756174
Killian Pogue <C17388633@mydit.ie>
2019-04-17 18:28:46 +0100

    started head for status display

--- java/src/ie/tudublin/StatusDisplay.java
+++ java/src/ie/tudublin/StatusDisplay.java
@@ -7,29 +7,6 @@ public class StatusDisplay extends MechObject
         super(mech, x, 0, 0, 0);
     }
 
-    boolean slideDone = false;
-
-    public void slideUP()
-    {
-        float i = 0;
-        for(i = - pos.x ; i <= 20 ; i++)
-        {
-            update();
-            mech.stroke(200);
-            //mech.rect(20, mech.height /2 , pos.x, (mech.height / 2) -20);
-            mech.rect(i, mech.height/2, pos.x, (mech.height / 2) -20);
-        }
-        if(i == 20)
-        {
-            slideDone = true;
-        }
-        if(slideDone == true)
-        {
-            render();
-        }
-        
-    }
-
 
     @Override
     public void update() {
@@ -45,8 +22,19 @@ public class StatusDisplay extends MechObject
         mech.rect(20, mech.height /2 , pos.x, (mech.height / 2) -20);
         mech.stroke(255);
         mech.noFill();
-        //mech.line((pos.x/2) + 10, mech.height - (mech.height/4) -10,(pos.x/2) + 10, mech.height - (mech.height/4)  + 10);
-       
+
+        // mech model
+        // Head
+        mech.line(pos.x/2, mech.height - (mech.height/4) - 90, (pos.x/2) - 10, mech.height - (mech.height/4) - 100);
+        mech.line((pos.x/2) + 20, mech.height - (mech.height/4) - 90, (pos.x/2) + 30, mech.height - (mech.height/4) - 100);
+        mech.line(pos.x/2, mech.height - (mech.height/4) - 90, (pos.x/2) + 20, mech.height - (mech.height/4) - 90);
+        // Shoulders
+        mech.line((pos.x/2) - 30, mech.height - (mech.height/4) - 70, (pos.x/2) + 50, mech.height - (mech.height/4) - 70);
+        // Backbone
+        mech.line((pos.x/2) + 10, mech.height - (mech.height/4) - 90, (pos.x/2) + 10, mech.height - (mech.height/4) + 20);
+        // Hip
+        mech.line((pos.x/2) - 10, mech.height - (mech.height/4) + 20, (pos.x/2) + 30, mech.height - (mech.height/4) + 20);
+   
     }
 
 }
